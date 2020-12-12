17b1e8be7ab342ac4fe671cbd6dcbde6bd924d4d
Kevin Basto <bastokevindaniel@gmail.com>
2020-12-11 20:20:12 -0600

    logo implemented

---
+++ src/components/header/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Logo.css'
+import {ReactComponent as Omnilogo} from './omnicare_web.svg'
+
+function Logo() {
+    return (
+        <div className="col-3 col-sm-6 col-md-3 d-none d-sm-block">
+            <div className="row no-gutters d-flex justify-content-center align-items-center logo">
+                <div className="logo_container">
+                    <Omnilogo /><span>mnicare</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Logo
