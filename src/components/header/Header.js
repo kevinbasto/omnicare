2383a2ec603ac22b79206b5a21fc322bd7596d9a
Kevin Basto <bastokevindaniel@gmail.com>
2020-12-11 20:20:01 -0600

    header implemented

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import './Header.css';
 
+//subcomponents
+import Navbar from './Navbar';
+import SearchBar from './Searchbar';
+import Logo from './Logo';
+
 function Header() {
     return (
-        <header className="row">
-            <div className="col-3 d-none d-sm-block logo">
-                <p>Hello world!</p>
-            </div>
-            <div className="col-6 d-none d-sm-block searchbar">
-                <p>Hello world!</p>
-            </div>
-            <div className="col-12 col-sm-3 menu">
-                <p>Hello world!</p>
-            </div>
+        <header className="d-flex row no-gutters header">
+            <Logo />
+            <SearchBar />
+            <Navbar />
         </header>
     )
 }
