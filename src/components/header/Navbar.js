558e5acfb769e0c875153818671fafd56a6cc318
Kevin Basto <bastokevindaniel@gmail.com>
2020-12-11 20:20:21 -0600

    navbar created

---
+++ src/components/header/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <div className="col-12 col-sm-6 col-md-3">
+            <div className="row no-gutters d-flex justify-content-center align-items-center navigation">
+                <div>Hello world!</div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
