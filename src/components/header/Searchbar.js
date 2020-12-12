db311aa91b9dace4756028e2afc769aa1abe4ce2
Kevin Basto <bastokevindaniel@gmail.com>
2020-12-11 20:20:39 -0600

    searchbar implemented

---
+++ src/components/header/Searchbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Searchbar.css'
+
+function Searchbar() {
+    return (
+        <div className="col-6 d-none d-md-block ">
+            <div className="row no-gutters d-flex justify-content-center align-items-center searchbar">
+                <div>Hello world!</div>
+            </div>
+        </div>
+    )
+}
+
+export default Searchbar
