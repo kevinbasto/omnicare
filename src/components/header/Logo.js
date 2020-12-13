2ad6f75ae34b04c8b0627a9e128ab60f075ebdfd
Kevin Basto <bastokevindaniel@gmail.com>
2020-12-12 22:05:19 -0600

    logo corrections

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -6,7 +6,7 @@ function Logo() {
     return (
         <div className="col-3 col-sm-6 col-md-3 d-none d-sm-block">
             <div className="row no-gutters d-flex justify-content-center align-items-center logo">
-                <div className="logo_container">
+                <div className="logo_container align-items-center">
                     <Omnilogo /><p>mnicare</p>
                 </div>
             </div>
