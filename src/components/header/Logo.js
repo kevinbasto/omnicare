990deea7e21c393f618db75172de305bbda74ae2
Kevin Basto <bastokevindaniel@gmail.com>
2020-12-12 20:37:01 -0600

    logo changes

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -7,7 +7,7 @@ function Logo() {
         <div className="col-3 col-sm-6 col-md-3 d-none d-sm-block">
             <div className="row no-gutters d-flex justify-content-center align-items-center logo">
                 <div className="logo_container">
-                    <Omnilogo /><span>mnicare</span>
+                    <Omnilogo /><p>mnicare</p>
                 </div>
             </div>
         </div>
