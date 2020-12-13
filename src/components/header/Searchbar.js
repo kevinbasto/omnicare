cb94231e69e661d149fad9e61b4d074e7a780bdf
Kevin Basto <bastokevindaniel@gmail.com>
2020-12-12 22:05:27 -0600

    searchbar sketch

--- src/components/header/Searchbar.js
+++ src/components/header/Searchbar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Searchbar.css'
 
+//icons
+import {ReactComponent as SearchIcon } from '../icons/search-24px.svg'
+
 function Searchbar() {
     return (
         <div className="col-6 d-none d-md-block ">
             <div className="row no-gutters d-flex justify-content-center align-items-center searchbar">
-                <div>Hello world!</div>
+                <input className="search_field"/>
+                <button className="search_button"><SearchIcon /></button>
             </div>
         </div>
     )
