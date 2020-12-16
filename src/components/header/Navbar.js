6583530f21442085781fabed485fd2a0f6b4cf98
Kevin Basto <bastokevindaniel@gmail.com>
2020-12-16 10:18:27 -0600

    icon correction and implementation

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import './Navbar.css'
 
+//icons import
+import { ReactComponent as UserIcon } from '../icons/person-24px.svg'
+import { ReactComponent as SettingsIcon } from '../icons/settings-24px.svg'
+import { ReactComponent as ShoppingCartIcon } from '../icons/shopping_cart-24px.svg'
+
 function Navbar() {
     return (
         <div className="col-12 col-sm-6 col-md-3">
             <div className="row no-gutters d-flex justify-content-center align-items-center navigation">
                 <ul className="nav_menu">
-                    <li className="nav_option">1</li>
-                    <li className="nav_option">2</li>
-                    <li className="nav_option">3</li>
+                    <li className="nav_option"> <UserIcon /> </li>
+                    <li className="nav_option"> <SettingsIcon /> </li>
+                    <li className="nav_option"> <ShoppingCartIcon /> </li>
                 </ul>
             </div>
         </div>
