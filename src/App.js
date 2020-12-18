9123e6e0ec75ad39c51d6f13a3fc12cb5cd81f7a
Kevin Basto <bastokevindaniel@gmail.com>
2020-12-17 21:54:44 -0600

    footer created

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 //components
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div style= {{height: '100vh'}}>a</div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="row no-gutters d-flex footer">
+            
+        </div>
+    )
+}
+
+export default Footer
