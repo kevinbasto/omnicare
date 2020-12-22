531a20d2031b7abcecb45f673e64d2773dbfee08
Kevin Basto <bastokevindaniel@gmail.com>
2020-12-22 10:15:05 -0600

    header implemented

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Footer.css'
 
+//commons import
+import Logo from '../Commons/Logo'
+
 function Footer() {
     return (
         <div className="row no-gutters d-flex footer">
-            
+            <Logo>
+                <span className="logo_slogan">Life Matters</span>
+            </Logo>
         </div>
     )
 }
