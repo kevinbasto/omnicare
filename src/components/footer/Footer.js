475b0445960ca99ffbe61897ec894ff4dcf69b7f
Kevin Basto <bastokevindaniel@gmail.com>
2021-01-09 08:21:45 -0600

    components

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,9 +7,13 @@ import Logo from '../Commons/Logo'
 function Footer() {
     return (
         <div className="row no-gutters d-flex footer">
-            <Logo>
+            <Logo classes="col-12 col-md-4 d-none d-sm-block" >
                 <span className="logo_slogan">Life Matters</span>
             </Logo>
+            <div className="col-12 col-md-4 d-flex justify-content-center align-items-center flex-wrap flex-column">
+                <span className="legalNotice" >Todos los derechos reservados</span>
+                <span className="legalNotice" >Omnicare S de RL</span>
+            </div>
         </div>
     )
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,7 +9,7 @@ import Logo from '../Commons/Logo';
 function Header() {
     return (
         <header className="d-flex row no-gutters header">
-            <Logo />
+            <Logo classes="col-3 col-sm-6 col-md-3 d-none d-sm-block"/>
             <SearchBar />
             <Navbar />
         </header>
